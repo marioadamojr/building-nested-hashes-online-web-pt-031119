1917e0f3c2b480a01aa2857d45d804b435106c71
Mario Adamo Jr <marioadamojr@gmail.com>
2019-03-23 22:57:02 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,16 +2,16 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      patriarch: #"Lord Montague",
-      matriarch: #"Lady Montague",
-      hero: #"Romeo",
+      patriarch: ,#"Lord Montague",
+      matriarch: ,#"Lady Montague",
+      hero: ,#"Romeo",
       hero_friends: []#"Benvolio", "Mercutio"]
       },
     :capulet => {
       #your key/value pairs here!
-      patriarch: #"Lord Capulet",
-      matriarch: #"Lady Capulet",
-      heroine: #"Juliet",
+      patriarch: ,#"Lord Capulet",
+      matriarch: ,#"Lady Capulet",
+      heroine: ,#"Juliet",
       heroine_friends: []#"Steven", "Nurse"]
       }}
 
