302b88764927c6d71f6e1543be88406feb70cb42
Mario Adamo Jr <marioadamojr@gmail.com>
2019-03-23 23:19:16 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,9 +8,13 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
+        name: "Lady Montague"
+        age: 54
         },
       :hero => {
         #your key/value pairs here
+        name: "Romeo"
+        age: 15
         },
       :hero_friends => []
    },
